e71e1775c00a533c26c817e9db8b051f5332274a
nagasundram <nagasundram@gmail.com>
2018-09-04 11:32:14 +0530

    ui twks to timeline

--- js/timeline.js
+++ js/timeline.js
@@ -24,24 +24,23 @@ function getList() {
       var expenses = result.expenses;
       html = '';
       expenses.forEach(function(expense, index) {
-        console.log(expense)
-        // ["2018-09-03T14:22:00.000Z", "Groceries", 211, "HDFC Credit", "Shopping", "🥚 Mop, paste", "12.96056278982475,77.63648218482513", "", "https://app.box.com/s/gqhoblcfxr4yopplq6c8if5gka5dvfpa"]
-        var date = new Date(expense[0]).toDateString() + ' ' + new Date(expense[0]).toLocaleTimeString(),
+        var date = moment(expense[0]).format("DD-MM-YYYY HH:mm"),
           bill = expense[8].length > 0 ? '<span class="right"><a target="_blank" href="' + expense[8] + '"><i class="tiny material-icons">receipt</i></a></span>' : '',
           sourceColor = expense[3].includes('Credit')? 'red-text' : 'green-text';
-        var time = $("<time></time>").text(date.slice(4,-3)),
+        var time = $("<time></time>").text(date),
           source = $("<span></span>").text(expense[3]).attr('class', 'right ' + sourceColor),
           firstp = $("<p></p>").append(time).append(source),
           subCat = $("<span></span>").text(expense[1]),
           amount = $("<span></span>").append('<strong>₹ ' + expense[2] + '</strong>').attr('class', 'right'),
           secodp = $("<p></p>").append(subCat).append(amount),
-          thirdp = $("<p></p>").text(expense[5]).attr('class', 'top-fix').append(bill),
+          info = $("<span></span>").text(expense[5]).attr('class', 'info-txt'),
+          thirdp = $("<p></p>").append(info).append(bill),
           lidiv = $("<div></div>").append(firstp).append(secodp).append('<hr>').append(thirdp);
           var li;
           if(index <= 3){
-            li = $("<li class='in-view'></li>").html(lidiv).attr('data-after', CAT_ICONS[expense[4]]);
+            li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS[expense[4]]).attr('class', 'in-view ' + expense[3].replace(' ', ''));
           } else {
-            li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS[expense[4]]);
+            li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS[expense[4]]).attr('class', expense[3].replace(' ', ''));
           }
         $('#list .timeline ul').append(li)
       })
