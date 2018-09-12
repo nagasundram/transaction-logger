ef2a9df510a6ae4112a388db8814a15a6fc3bd14
nagasundram <nagasundram@gmail.com>
2018-09-12 08:52:18 +0530

    Bill preview in timeline

--- js/timeline.js
+++ js/timeline.js
@@ -9,8 +9,8 @@ $(function() {
       $("#list").show()
       $("#loading").show();
       getList();
-    })
-  });
+    });
+  })
 })
 
 function getList() {
@@ -25,8 +25,9 @@ function getList() {
       var expenses = result.expenses;
       expenses.forEach(function(expense, index) {
         var date = moment(expense[0]).format("DD-MM-YYYY HH:mm"),
-          bill = expense[8].length > 0 ? '<span class="right"><a target="_blank" href="' + expense[8] + '"><i class="tiny material-icons">receipt</i></a></span>' : '',
-          sourceColor = expense[3].includes('Credit')? 'red-text' : 'green-text';
+          // bill = expense[8].length > 0 ? '<span class="right"><a target="_blank" href="' + expense[8] + '"><i class="tiny material-icons">receipt</i></a></span>' : '',
+          bill = expense[8].length > 0 ? '<span class="right"><a class="billImgLink" href="javascript:;" data-imgurl="' + expense[8] + '"><i class="tiny material-icons">receipt</i></a></span>' : '',
+          sourceColor = expense[3].includes('Credit') ? 'red-text' : 'green-text';
         var time = $("<time></time>").text(date),
           source = $("<span></span>").text(expense[3]).attr('class', 'right ' + sourceColor),
           firstp = $("<p></p>").append(time).append(source),
@@ -36,19 +37,32 @@ function getList() {
           info = $("<span></span>").text(expense[5]).attr('class', 'info-txt'),
           thirdp = $("<p></p>").append(info).append(bill).attr('style', 'min-height: 20px;'),
           lidiv = $("<div></div>").append(firstp).append(secodp).append('<hr>').append(thirdp);
-          var li;
-          if(index <= 3){
-            li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS_IOS[expense[4]]).attr('class', 'in-view ' + expense[3].replace(' ', ''));
-          } else {
-            li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS_IOS[expense[4]]).attr('class', expense[3].replace(' ', ''));
-          }
+        var li;
+        if (index <= 3) {
+          li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS_IOS[expense[4]]).attr('class', 'in-view ' + expense[3].replace(' ', ''));
+        } else {
+          li = $("<li></li>").html(lidiv).attr('data-after', CAT_ICONS_IOS[expense[4]]).attr('class', expense[3].replace(' ', ''));
+        }
         $('#list .timeline ul').append(li);
       })
+      imageLinkActionListener();
       timlineAnimation();
     }
   });
 }
 
+function imageLinkActionListener() {
+  var imageLinks = $(".billImgLink");
+  imageLinks.each(function() {
+    imageLink = $(this);
+    imageLink.on('click', function(event) {
+      var imgurl = $(event.target).parent().data('imgurl');
+      $('#billImg').attr('src', imgurl);
+      $('#imgDownloadLink').attr('href', imgurl);
+      $('#imageModal').show();
+    })
+  })
+}
 
 function timlineAnimation() {
 
