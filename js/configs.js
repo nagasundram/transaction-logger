9fafdfbccb8cd615c206c103c79afbafcf383bf7
nagasundram <nagasundram@gmail.com>
2018-09-13 16:02:34 +0530

    Show and Delete Action (Only for today expenses)

--- js/configs.js
+++ js/configs.js
@@ -28,6 +28,7 @@ var CATEGORIES = {
   WRITE_URL = "https://script.google.com/macros/s/AKfycbzp29Qzo_oLjAgi2UnhkRDl798lXFiU99Jy-aqXIuuE8NF0Ejlq/exec?row=",
   READ_URL = "https://script.google.com/macros/s/AKfycbwiLibhxusQjgb4yl_3ue0_wY_NojiSRQI1KZOu7HZXMapFO2k/exec",
   CHART_URL = "https://script.google.com/macros/s/AKfycbzqbIwv3mExSH1I_kq3QiTiTvD85rXgI7uEWYnkjbe3JGJsnB0/exec",
+  ACTION_URL = "https://script.google.com/macros/s/AKfycbwclNzWz4lXRs_LyFGoW_maBzNcC52FonDOrsTMJ9n4ed20nk0/exec",
   CAT_ICONS = {
     'Food' : 'restaurant',
     'Travel': 'directions_car',
