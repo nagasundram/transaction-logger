32c0ba5d4781c40d2dcee15dda0bcbb467c61c54
nagasundram <nagasundram@gmail.com>
2019-03-01 14:37:13 +0530

    Pie for Budget screen

--- js/budget.js
+++ js/budget.js
@@ -1,71 +1,112 @@
 $(function() {
   $(document).ready(function() {
-    $('#budgetTgr').on('click', function(e) {
-      $('#addTransaction').show();
-      $('#expensesMap').show();
-      $('#listTgr').show();
+    $("#budgetTgr").on("click", function(e) {
+      $("#addTransaction").show();
+      $("#expensesMap").show();
+      $("#listTgr").show();
       $("#form_card, #suggestions").hide();
       $("#map").hide();
-      $("#list").hide()
+      $("#list").hide();
       $("#budgetTgr").hide();
       $("#budget-container").show();
       $("#loading").show();
-      $('#summaryArea').removeClass('show-summary');
-      $('#today').removeClass('show-today');
+      $("#summaryArea").removeClass("show-summary");
+      $("#today").removeClass("show-today");
       getAndShowBudget();
     });
-    $('#balanceSwitch').on('click', function(e){
+    $("#balanceSwitch").on("click", function(e) {
       var bswitch = $(this),
-        budgetBars = $('#budget').find('.progress'),
-        planned, rightEnd;
+        budgetBars = $("#budget").find(".progress"),
+        planned,
+        rightEnd;
       $.each(budgetBars, function(index, bar) {
-        planned = $($(bar).children().children()[0]);
-        rightEnd = $($(bar).children().children()[1]);
+        planned = $(
+          $(bar)
+            .children()
+            .children()[0]
+        );
+        rightEnd = $(
+          $(bar)
+            .children()
+            .children()[1]
+        );
         rightEnd.text(parseFloat(planned.text()) - parseFloat(rightEnd.text()));
       });
-      if(bswitch.html() == 'Spent'){
-        bswitch.html('Balance');
+      if (bswitch.html() == "Spent") {
+        bswitch.html("Balance");
       } else {
-        bswitch.html('Spent')
+        bswitch.html("Spent");
       }
     });
   });
 });
 
 function getAndShowBudget() {
-  var budgetDiv = $('#budget');
-  $('#budget-container').hide();
+  var budgetDiv = $("#budget");
+  $("#budget-container").hide();
   budgetDiv.empty();
   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbxb2XVYjTfM9CYNEvlpOHmj5QIR_-t3utN4gBMLwf1WLUNhPIs/exec",
-    type: 'GET',
+    url:
+      "https://script.google.com/macros/s/AKfycbxb2XVYjTfM9CYNEvlpOHmj5QIR_-t3utN4gBMLwf1WLUNhPIs/exec",
+    type: "GET",
     success: function(result) {
       var budgets = result.budgets;
-      $('#loading').hide();
+      $("#loading").hide();
       budgets.forEach(function(budget, index) {
         var used = budget[1] - budget[2],
           percentage = ((used / budget[1]) * 100).toFixed(2),
           cat = budget[0],
           intPer = parseInt(percentage),
-          greenCat = ['Chit', 'Mutual Fund', 'Home', 'Savings'],
+          greenCat = ["Chit", "Mutual Fund", "Home", "Savings"],
           color;
-          if (greenCat.includes(cat)){
-            color = (intPer <= 50) ? 'red' : (intPer <= 80) ? 'orange' : 'green';
-          } else {
-            color = (intPer <= 50) ? 'green' : (intPer <= 80) ? 'orange' : 'red';
-          }
-          var catDiv = $("<div></div>").text(cat + ': ').attr('class', 'white-text col s3'),
-          perDiv = $("<div></div>").text(intPer + '%').attr('class', 'white-text col s3'),
-          prog = $("<div></div>").attr('class', 'determinate ' + color).attr('style', 'width: ' + intPer + '%'),
-          limitDiv = $("<div></div>").attr('class', 'col s6').attr('style', 'text-align: left').text(budget[1]),
-          usedDiv = $("<div></div>").attr('class', 'col s6').attr('style', 'text-align: right').text(used),
-          limitRow = $("<div></div>").attr('class', 'row').attr('style', 'position: relative; z-index: 9;').append(limitDiv).append(usedDiv),
-          progressCon = $("<div></div>").attr('class', 'progress white col s6'),
+        if (greenCat.includes(cat)) {
+          color = intPer <= 50 ? "red" : intPer <= 80 ? "orange" : "green";
+        } else {
+          color = intPer <= 50 ? "green" : intPer <= 80 ? "orange" : "red";
+        }
+        var catDiv = $("<div></div>")
+            .text(cat + ": ")
+            .attr("class", "white-text col s3")
+            .attr("style", "margin-top: 8px"),
+          perDiv = $("<div></div>")
+            .text(intPer + "%")
+            .attr("class", "white-text col s3")
+            .attr("style", "margin-top: 8px"),
+          prog = $("<div></div>")
+            .attr("class", "determinate " + color)
+            .attr("style", "width: " + intPer + "%"),
+          limitDiv = $("<div></div>")
+            .attr("class", "col s6")
+            .attr("style", "text-align: left")
+            .text(budget[1]),
+          usedDiv = $("<div></div>")
+            .attr("class", "col s6")
+            .attr("style", "text-align: right")
+            .text(used),
+          limitRow = $("<div></div>")
+            .attr("class", "row")
+            .attr("style", "position: relative; z-index: 9;")
+            .append(limitDiv)
+            .append(usedDiv),
+          progressCon = $("<div></div>").attr("class", "progress white col s6"),
           progressDiv = progressCon.append(limitRow).append(prog),
-          rowDiv = $("<div></div>").attr('class', 'row').append(catDiv).append(perDiv).append(progressDiv);
+          pieDiv = $("<span>")
+            .attr("id", "pie" + index)
+            .addClass("pie"),
+          perPieDiv = perDiv.prepend(pieDiv),
+          rowDiv = $("<div></div>")
+            .attr("class", "row")
+            .append(catDiv)
+            .append(perPieDiv)
+            .append(progressDiv);
         budgetDiv.append(rowDiv);
-        $('#budget-container').show();
+        $("#budget-container").show();
+        $("#pie" + index).sparkline([percentage, 100 - percentage], {
+          type: "pie",
+          sliceColors: [color, "#000"],
+          offset: -90
+        });
       });
     }
   });
-};
+}
